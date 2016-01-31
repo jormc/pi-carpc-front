e59c9202f2d66f45ef1781fb75bd9b6a3e87c18d
Jordi MC <jordi@jormc.es>
2016-01-31 14:13:15 +0100

    Basic AngularJS structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name piCarpcFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the piCarpcFrontApp
+ */
+angular.module('piCarpcFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name piCarpcFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the piCarpcFrontApp
+ */
+angular.module('piCarpcFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
